eb1abf5f5d8d2e03f7dc82db0fb31b6ca7dcc1fc
Brian List <brianl@csgpro.com>
2014-05-27 09:10:53 -0700

    Tried adding createUser method and associate button to that

--- Application/HTML5_BDL1/app/admin/admin.js
+++ Application/HTML5_BDL1/app/admin/admin.js
@@ -22,7 +22,24 @@
                 .then(function () { log('Activated Admin View'); });
         }
 
+        function TESTcreateUser()
+        {
+            return tkService.createUser("Dude3", "password", "name 3", "dude3@dude3.com", "1")
+                .success(function(response) {
+                    common.$timeout(function () {
+                        vm.user = response.user;
+                    })
+                    return null;
+                }).error(function (error) {
+                    logError('Error: ' + error);
+                    vm.user = {};
+                    return null;
+                });
+        }
+
         function getUser() {
+            TESTcreateUser();
+            /*
             // https://csgprohackathonapi.azurewebsites.net/api/users/2
             if (vm.userId && !isNaN(vm.userId)) {
                 return tkService.getUserById(vm.userId)
@@ -37,7 +54,10 @@
                         return null;
                     });
             }
+            */
         }
+
+
 
     }
 })();
--- Application/HTML5_BDL1/app/services/tkService.js
+++ Application/HTML5_BDL1/app/services/tkService.js
@@ -14,17 +14,25 @@
         var $q = common.$q;
         var $http = common.$http;
         var baseServiceUrl = cfg.config.baseServiceUrl;
-        var url = 'https://csgprohackathonapi.azurewebsites.net/api/' + 'policies';
+        var url = 'https://csgprohackathonapi.azurewebsites.net/api/';
         var service = {
             getUserById: getUserById,
-            //createUser: createUser,
+            createUser: createUser,
             //updateUser: updateUser
         };
 
         return service;
 
+        function createUser(userName, password, name, email, timezone) {
+            var poststr = url + "users?Username=" + userName +
+                "&Password=" + password + "&Name=" + name + "&email=" + email +
+                "&timezoneid=" + timezone + "&format=json&callId=" + common.generateGuid()
+            return $http.post(poststr);
+        }
+
         function getUserById(userId) {
-            return $http.get(url + "/" + userId + "?format=json&callId=" + common.generateGuid());
+            var getstr = url + "users/" + userId + "?format=json&callId=" + common.generateGuid();
+            return $http.get(getstr);
         }
     }
 })();

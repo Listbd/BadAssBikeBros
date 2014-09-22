72f959b4f53c0a98c8cee6c8aa7a1341576183a0
Dean Davidson <ddavidson85@gmail.com>
2014-09-22 15:27:30 -0700

    Added ngAnimate to project

--- Application/WorldsBestTimeTrackingApp/SleekPeek2/app/app.js
+++ Application/WorldsBestTimeTrackingApp/SleekPeek2/app/app.js
@@ -1,7 +1,7 @@
 ﻿(function () {
     'use strict';
 
-    var app = angular.module('app', ['ngCookies', 'ngRoute', 'ngGrid', 'ngResource', 'angular-loading-bar', 'common']);
+    var app = angular.module('app', ['ngCookies', 'ngRoute', 'ngGrid', 'ngResource', 'ngAnimate', 'angular-loading-bar', 'common']);
 
     app.config(function ($routeProvider) {
         $routeProvider
